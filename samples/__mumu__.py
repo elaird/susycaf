cfda2fe951a3071e1d7f4c7fb679ae2671de52b2
Edward Laird <elaird@gmail.com>
2011-11-29 23:31:15 +0000

    add skims

--- samples/__mumu__.py
+++ samples/__mumu__.py
@@ -9,3 +9,11 @@ mumu.add("DoubleMu.Run2011A-May10ReReco-v1.AOD.job662", '%s/henning/ICF/automate
 mumu.add("DoubleMu.Run2011A-PromptReco-v4.AOD.job664", '%s/dburton/ICF/automated/2011_11_11_14_54_49/DoubleMu.Run2011A-PromptReco-v4.AOD")'%srm,   lumi = 851.5)
 mumu.add("DoubleMu.Run2011A-PromptReco-v6.AOD.job665", '%s/bainbrid/ICF/automated/2011_11_12_15_35_18/DoubleMu.Run2011A-PromptReco-v6.AOD")'%srm,  lumi = 617.3)
 mumu.add("DoubleMu.Run2011B-PromptReco-v1.AOD.job666", '%s/bm409/ICF/automated/2011_11_11_13_34_14/DoubleMu.Run2011B-PromptReco-v1.AOD")'%srm,     lumi = 2545.2)
+
+l = 'utils.fileListFromDisk(isDirectory = False, location = '
+dir = '/vols/cms02/elaird1/29_skims/08_mumu/v1'
+mumu.add("DoubleMu.Run2011A-05Aug2011-v1.AOD.job663_skim",   '%s"%s/DoubleMu.Run2011A-05Aug2011-v1.AOD.job663_*_skim.root",  )'%(l, dir), lumi = 3.567000e+02)
+mumu.add("DoubleMu.Run2011A-May10ReReco-v1.AOD.job662_skim", '%s"%s/DoubleMu.Run2011A-May10ReReco-v1.AOD.job662_*_skim.root",)'%(l, dir), lumi = 2.023000e+02)
+mumu.add("DoubleMu.Run2011A-PromptReco-v4.AOD.job664_skim",  '%s"%s/DoubleMu.Run2011A-PromptReco-v4.AOD.job664_*_skim.root", )'%(l, dir), lumi = 8.515000e+02)
+mumu.add("DoubleMu.Run2011A-PromptReco-v6.AOD.job665_skim",  '%s"%s/DoubleMu.Run2011A-PromptReco-v6.AOD.job665_*_skim.root", )'%(l, dir), lumi = 6.173000e+02)
+mumu.add("DoubleMu.Run2011B-PromptReco-v1.AOD.job666_skim",  '%s"%s/DoubleMu.Run2011B-PromptReco-v1.AOD.job666_*_skim.root", )'%(l, dir), lumi = 2.545200e+03)
