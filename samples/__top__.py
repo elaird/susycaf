fd975ac0c94fe7d326b36bc71b4377a0c25934d7
Burton Betchart <betchart@gmail.com>
2011-12-09 07:08:37 +0000

    fix typos

--- samples/__top__.py
+++ samples/__top__.py
@@ -6,7 +6,7 @@ srm_burt = srm + '/bbetchar/ICF/automated'
 singleT = srm_burt + '/2011_12_08_08_39_30/%s_TuneZ2_%s_7TeV-powheg-tauola.Fall11-PU_S6_START42_V14B-v1.AODSIM/")'
 
 # Fall2011 reprocessing of Summer 2011
-top.add("ttbar_mg", '/2011_12_08_08_31_06/")'%srm_burt , xs = {"LO":94.76, "guessNLO":157.5 }['guessNLO'] )
+top.add("ttbar_mg", '%s/2011_12_08_08_31_06/")'%srm_burt , xs = {"LO":94.76, "guessNLO":157.5 }['guessNLO'] )
 top.add("top_s_ph", singleT%('T','s-channel'), xs = 2.341 )
 top.add("top_t_ph", singleT%('T','t-channel'), xs = 35.72 )
 top.add("top_tW_ph", singleT%('T','tW-channel-DS'), xs = 7.104 )
@@ -15,5 +15,5 @@ top.add("tbar_t_ph", singleT%('Tbar','t-channel'), xs = 18.43 )
 top.add("tbar_tW_ph", singleT%('Tbar','tW-channel-DS'), xs = 7.108 )
 
 # Summer2011
-top.add("tt_tauola_fj_mg",'%s/2011_07_20_22_27_52/")'%srm_burt, xs = xs = { "LO":94.76, "guessNLO":157.5}["guessNLO"] )
+top.add("tt_tauola_fj_mg",'%s/2011_07_20_22_27_52/")'%srm_burt, xs = { "LO":94.76, "guessNLO":157.5}["guessNLO"] )
 top.add("tt_tauola_fj", '%s/arlogb//ICF/automated/2011_07_11_17_17_07/")'%srm, xs = { "LO":94.76, "guessNLO":157.5}["guessNLO"])
