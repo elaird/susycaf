fe807da5b8b40c6eb90786944443e1937ba1b9ad
Burton Betchart <betchart@gmail.com>
2012-02-14 22:41:24 +0000

    Do not use Chamonix12: lack gen mother information

--- samples/__top16__.py
+++ samples/__top16__.py
@@ -6,7 +6,7 @@ srm_burt = srm + '/bbetchar/ICF/automated'
 #singleT = srm_burt + '/2011_12_08_08_39_30/%s_TuneZ2_%s_7TeV-powheg-tauola.Fall11-PU_S6_START42_V14B-v1.AODSIM/")'
 
 # Fall2011 reprocessing of Summer 2011
-top16.add("ttj_mg", '%s/2012_02_09_15_32_20/")'%srm_burt , xs = {"LO":94.76, "guessNLO":157.5 }['guessNLO'] )
+top16.add("ttj_mg", '%s/2012_02_14_20_57_14/")'%srm_burt , xs = {"LO":94.76, "guessNLO":157.5 }['guessNLO'] )
 #top16.add("top_s_ph", singleT%('T','s-channel'), xs = 2.341 )
 #top16.add("top_t_ph", singleT%('T','t-channel'), xs = 35.72 )
 #top16.add("top_tW_ph", singleT%('T','tW-channel-DS'), xs = 7.104 )
