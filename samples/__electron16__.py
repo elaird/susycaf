bf2b29b01f05c08fc1a239e388ee892585a1577c
Burton Betchart <betchart@gmail.com>
2012-02-19 22:27:03 +0000

    new electron and muon file addresses

--- samples/__electron16__.py
+++ samples/__electron16__.py
@@ -2,6 +2,5 @@ from supy.samples import SampleHolder
 from supy.sites import srm
 electron16 = SampleHolder()
 
-electron16.add("SingleEl.Run2011A.1", '%s/bbetchar//ICF/automated/2012_02_09_06_16_46/")'%srm, lumi = 9999 )
-electron16.add("SingleEl.Run2011A.2", '%s/bbetchar//ICF/automated/2012_02_09_06_22_00/")'%srm, lumi = 9999 )
-electron16.add("SingleEl.Run2011B",   '%s/bbetchar//ICF/automated/2012_02_09_06_27_23/")'%srm, lumi = 9999 )
+electron16.add("SingleEl.Run2011A", '%s/bbetchar//ICF/automated/2012_02_18_01_31_30/")'%srm, lumi = 9999 )
+electron16.add("SingleEl.Run2011B", '%s/bbetchar//ICF/automated/2012_02_18_02_01_43/")'%srm, lumi = 9999 )
--- samples/__muon16__.py
+++ samples/__muon16__.py
@@ -2,6 +2,5 @@ from supy.samples import SampleHolder
 from supy.sites import srm
 muon16 = SampleHolder()
 
-muon16.add("SingleMu.2011A.1", '%s/bbetchar//ICF/automated/2012_02_09_05_54_46/")'%srm, lumi = 435.1397 )
-muon16.add("SingleMu.2011A.2", '%s/bbetchar//ICF/automated/2012_02_09_06_02_10/")'%srm, lumi = 1687.8672 )
-muon16.add("SingleMu.2011B",   '%s/bbetchar//ICF/automated/2012_02_09_06_09_26/")'%srm, lumi = 2598.4747 )
+muon16.add("SingleMu.2011A", '%s/bbetchar//ICF/automated/2012_02_18_00_42_18/")'%srm, lumi = 9999 )
+muon16.add("SingleMu.2011B", '%s/bbetchar//ICF/automated/2012_02_18_00_28_45/")'%srm, lumi = 9999 )
