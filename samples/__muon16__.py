0b04bf63e1e9fdd5d745736b53a01ec5cef5d644
Burton Betchart <betchart@gmail.com>
2012-02-23 01:55:08 +0000

    fewer lumis per job (30, rather than 100) since grid aborts after 24h

--- samples/__muon16__.py
+++ samples/__muon16__.py
@@ -2,5 +2,5 @@ from supy.samples import SampleHolder
 from supy.sites import srm
 muon16 = SampleHolder()
 
-muon16.add("SingleMu.2011A", '%s/bbetchar//ICF/automated/2012_02_18_00_42_18/")'%srm, lumi = 9999 )
-muon16.add("SingleMu.2011B", '%s/bbetchar//ICF/automated/2012_02_18_00_28_45/")'%srm, lumi = 9999 )
+muon16.add("SingleMu.2011A", '%s/bbetchar//ICF/automated/2012_02_23_02_37_07/")'%srm, lumi = 9999 )
+muon16.add("SingleMu.2011B", '%s/bbetchar//ICF/automated/2012_02_22_00_30_32/")'%srm, lumi = 9999 )
