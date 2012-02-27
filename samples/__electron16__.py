5dbfe9753de59b2948890acccef9b2e0bd260cb5
Burton Betchart <betchart@gmail.com>
2012-02-27 02:28:47 +0000

    pixel-based lumi for data samples v16.1

--- samples/__electron16__.py
+++ samples/__electron16__.py
@@ -2,5 +2,5 @@ from supy.samples import SampleHolder
 from supy.sites import srm
 electron16 = SampleHolder()
 
-electron16.add("SingleEl.Run2011A", '%s/bbetchar//ICF/automated/2012_02_18_01_31_30/")'%srm, lumi = 9999 )
-electron16.add("SingleEl.Run2011B", '%s/bbetchar//ICF/automated/2012_02_18_02_01_43/")'%srm, lumi = 9999 )
+electron16.add("SingleEl.2011A", '%s/bbetchar//ICF/automated/2012_02_18_01_31_30/")'%srm, lumi = 2284.4995 )
+electron16.add("SingleEl.2011B", '%s/bbetchar//ICF/automated/2012_02_18_02_01_43/")'%srm, lumi = 2735.2943 )
