cb5c2e248f76c974ebe65b18da7584897202d4ca
Burton Betchart <betchart@gmail.com>
2012-02-28 21:59:46 +0000

    skip corrupt files

--- samples/__ewk16__.py
+++ samples/__ewk16__.py
@@ -6,6 +6,6 @@ srm_burt = srm+'/bbetchar/ICF/automated'
 
 #Fall 11
 # ewk16.add("wbb_mg", ) #pending
-corrupt = ','.join('"SusyCAF_Tree_%d"'%d for d in [1287,1603,1286,1092]).join(['[',']'])
+corrupt = ','.join('"SusyCAF_Tree_%d"'%d for d in [1287,1603,1286,1092,1237,1509,1415,1279,1385,1266,1150,1483,288]).join(['[',']'])
 ewk16.add("wj_lv_mg", '%s/2012_02_18_01_38_16/", itemsToSkip = %s)'%(srm_burt,corrupt), xs = {"LO":27770.0, "guessNLO":0.9 * 55854}['guessNLO'] )
 ewk16.add("dyj_ll_mg", '%s/2012_02_14_21_04_44/")'%srm_burt, xs = {"LO":2475.0, "guessNLO":None}['LO'] )
--- samples/__muon16__.py
+++ samples/__muon16__.py
@@ -2,6 +2,6 @@ from supy.samples import SampleHolder
 from supy.sites import srm
 muon16 = SampleHolder()
 
-corrupt = ','.join('"SusyCAF_Tree_%d"'%d for d in [1125,290,339,428,579,606,742,721]).join(['[',']'])
-muon16.add("SingleMu.2011A", '%s/bbetchar//ICF/automated/2012_02_23_02_37_07/", itemsToSkip=%s)'%(srm,corrupt), lumi = 2281.1693 ) # 2287.3459 including corrupt
+corrupt = ','.join('"SusyCAF_Tree_%d"'%d for d in [1125,290,339,428,579,606,742,721,605,206,1054,895,389][-1:]).join(['[',']'])  # 2287.3459 ) # corrupt included
+muon16.add("SingleMu.2011A", '%s/bbetchar//ICF/automated/2012_02_23_02_37_07/", itemsToSkip=%s)'%(srm,corrupt),       lumi =  2266.5375 ) # corrupt omitted
 muon16.add("SingleMu.2011B", '%s/bbetchar//ICF/automated/2012_02_22_00_30_32/")'%srm, lumi = 2738.0194 )
