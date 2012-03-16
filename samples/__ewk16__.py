54dafc3524d344ac49da8990d9400b5cb8523920
Burton Betchart <betchart@gmail.com>
2012-03-16 05:35:36 +0000

    reestimate Wjets xs

--- samples/__ewk16__.py
+++ samples/__ewk16__.py
@@ -6,5 +6,6 @@ srm_burt = srm+'/bbetchar/ICF/automated'
 
 #Fall 11
 # ewk16.add("wbb_mg", ) #pending
-ewk16.add("wj_lv_mg", '%s/2012_02_29_14_33_59/")'%srm_burt, xs = {"LO":27770.0, "guessNLO":0.9 * 55854}['guessNLO'] )
+ewk16.add("wj_lv_mg", '%s/2012_02_29_14_33_59/")'%srm_burt, xs = {"LO":27770.0, "guessNLO": 37700}['guessNLO'] )
 ewk16.add("dyj_ll_mg", '%s/2012_02_14_21_04_44/")'%srm_burt, xs = {"LO":2475.0, "guessNLO":None}['LO'] )
+
