153e9a63cdf086d36ad69ca5a8459d80017e953c
Edward Laird <elaird@gmail.com>
2012-03-18 20:11:03 +0100

    move susycaf configuration settings

---
+++ configuration.py
@@ -0,0 +1,53 @@
+import supy
+
+class configuration(supy.configuraion) :
+    def mainTree(self) :
+        return ("susyTree","tree")
+
+    def otherTreesToKeepWhenSkimming(self) :
+        return [("lumiTree","tree")]
+
+    def trace(self) :
+        return True
+
+    def useCachedFileLists(self) :
+        return True
+
+    def cppFiles(self) :
+        return ["cpp/linkdef.cxx"]
+
+    def hadd(self) :
+        return ['hadd', whereami()+'/run/phaddy'][1]
+
+    def dictionariesToGenerate(self) :
+        return [
+            ("pair<string,bool>", "string"),
+            ("map<std::string,bool>", "string;map"),
+            ("pair<string,string>", "string"),
+            ("map<std::string,string>", "string;map"),
+            ("ROOT::Math::Cartesian3D<float>", "Math/Point3D.h"),
+            ("ROOT::Math::DisplacementVector3D<ROOT::Math::Cartesian3D<float>,ROOT::Math::DefaultCoordinateSystemTag>", "Math/Vector3D.h"),
+            ("vector<ROOT::Math::DisplacementVector3D<ROOT::Math::Cartesian3D<float>,ROOT::Math::DefaultCoordinateSystemTag> >", "vector;Math/Vector3D.h"),
+            ("ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<float>,ROOT::Math::DefaultCoordinateSystemTag>", "Math/Point3D.h"),
+            ("vector<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<float>,ROOT::Math::DefaultCoordinateSystemTag> >", "vector;Math/Point3D.h"),
+            #ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<float> > etc. is addressed in linkdef.cxx
+            ("vector<ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<float> > >", "vector;Math/LorentzVector.h"),
+            ("vector< vector< float > >", "vector"),
+            ("vector< vector< unsigned int> >", "vector"),
+            ("vector< vector< int> >", "vector"),
+            ]
+
+    def whereami(self) :
+        return max('/'.join(__file__.split('/')[:-1]), '.')
+
+    def detectorSpecs(self) :
+        return {
+            "cms": {"etaBE": 1.479, #from CMS PAS EGM-10-005
+                    "barrelEtaMax": 1.4442,
+                    "endcapEtaMin": 1.560,
+                    "CaloSubdetectors": ["Eb", "Ee", "Hbhe", "Hf"],
+                    "PFSubdetectors": ["Ecal", "Hcal", "Hfem", "Hfhad", "Ps"],
+                    "CaloRecHitCollections": [""],
+                    "PFRecHitCollections": ["","cluster"],
+                    },
+            }
