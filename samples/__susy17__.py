bd62601c51e266a4b031ac607ca460e0a68b9920
Edward Laird <elaird@gmail.com>
2012-09-21 17:02:34 +0200

    1-file samples

--- samples/__susy17__.py
+++ samples/__susy17__.py
@@ -4,3 +4,6 @@ pnfs = pnfs()
 susy17 = SampleHolder()
 
 susy17.add("T2tt_8.job351", '%s/yeshaq//ICF/automated/2012_09_04_23_14_16/")'%pnfs, xs = 1.0) #dummy XS
+susy17.add("T2tt_8.job351_1", '["/home/elaird/141_ntuples/job351/SusyCAF_Tree_1000_1_Bim.root"]', xs = 1.0) #dummy XS; [(500.0, 100.0), (775.0, 200.0)]
+susy17.add("T2tt_500_100", '["/home/elaird/141_ntuples/job351/T2tt_8.job351_1_1_0_skim.root"]', xs = 0.0855) #/pb
+
