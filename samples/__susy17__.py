97f827888d92ec5e803ef173df775589e87032ce
Edward Laird <elaird@gmail.com>
2012-09-27 14:05:16 +0200

    more signal

--- samples/__susy17__.py
+++ samples/__susy17__.py
@@ -5,6 +5,7 @@ susy17 = SampleHolder()
 
 susy17.add("T2tt_8.job351", '%s/yeshaq//ICF/automated/2012_09_04_23_14_16/")'%pnfs, xs = 1.0) #dummy XS
 susy17.add("T2tt_8.job351_1", '["/uscms/home/elaird/141_ntuples/job351/SusyCAF_Tree_1000_1_Bim.root"]', xs = 1.0) #dummy XS; [(500.0, 100.0), (775.0, 200.0)]
-susy17.add("T2tt_500_100", '["/uscms/home/elaird/141_ntuples/job351/T2tt_8.job351_500_100_partial.root"]', xs = 0.0855) #/pb
+susy17.add("T2tt_500_0",   '["/uscms/home/elaird/141_ntuples/job351/T2tt_8.job351_500_0.root"]', xs = 0.0855) #/pb
+susy17.add("T2tt_500_100", '["/uscms/home/elaird/141_ntuples/job351/T2tt_8.job351_500_100.root"]', xs = 0.0855) #/pb
 susy17.add("T2tt_500_300", '["/uscms/home/elaird/141_ntuples/job351/T2tt_8.job351_500_300.root"]', xs = 0.0855) #/pb
 
