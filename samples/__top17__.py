932ea4968268d65dc0451dfc1599e4656fbe4ef4
Edward Laird <elaird@gmail.com>
2012-09-30 12:42:33 +0200

    skims

--- samples/__top17__.py
+++ samples/__top17__.py
@@ -8,6 +8,8 @@ top17.add("tt_8_mg.job188", '%s/clucas//ICF/automated/2012_06_05_09_30_46/TTJets
 top17.add("tt_8_mg.job315", '%s/clucas/ICF/automated/2012_08_17_14_42_02/")'%pnfs, xs = 234.)
 top17.add("tt_8_mg.job315_1", '["/uscms/home/elaird/141_ntuples/job315/SusyCAF_Tree_100_1_h3D.root"]', xs = 234.)
 top17.add("tt_8_mg.job315_zeroNu_50Met", '["/uscms/home/elaird/141_ntuples/job315/tt_8_mg.job315_zeroNu_50Met.root"]', xs = 234.*(44005./6923750))
+top17.add("tt_8_mg.job315_1_oneNu_100Met", '["/uscms/home/elaird/141_ntuples/job315/tt_8_mg.job315_oneNu_100Met.root"]', xs = 234.*(1465./20000.))
+top17.add("tt_8_mg.job315_1_twoNu_100Met", '["/uscms/home/elaird/141_ntuples/job315/tt_8_mg.job315_twoNu_100Met.root"]', xs = 234.*(510./20000.))
 
 top17.add("ttz_8_mg.job269", '%s/zmeng//ICF/automated/2012_07_20_15_24_50")'%pnfs, xs = 0.172) #xs from PREP
 top17.add("ttz_8_mg.job269_1", '["/uscms/home/elaird/141_ntuples/job269/SusyCAF_Tree_11_1_7LX.root"]', xs = 0.172) #xs from PREP
