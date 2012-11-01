a243eb50c061691691d60b60c7e78febcd0478a9
Yossof Eshaq <yossofeshaq@gmail.com>
2012-10-31 20:35:02 -0500

    adding sms samples

---
+++ samples/__susy17__.py
@@ -0,0 +1,23 @@
+from supy.samples import SampleHolder
+from supy.sites import pnfs
+susy17 = SampleHolder()
+pnfs = pnfs()
+
+susy17.add("t1.yos", '%s/yeshaq//ICF/automated/2012_07_02_17_24_20/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
+susy17.add("t2.yos", '%s/yeshaq//ICF/automated/2012_07_10_16_25_41/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
+susy17.add("t2tt.yos", '%s/yeshaq//ICF/automated/2012_07_10_16_39_39/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
+susy17.add("t2bb.yos", '%s/yeshaq//ICF/automated/2012_07_10_16_50_17/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
+susy17.add("t1tttt.yos", '%s/yeshaq//ICF/automated/2012_07_10_17_01_28/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
+susy17.add("t1bbbb.yos", '%s/yeshaq//ICF/automated/2012_07_10_17_11_44/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
+susy17.add("t1tttt.chr", '%s/clucas//ICF/automated/2012_06_04_18_26_52/")'%pnfs, xs = 1.0) #dummy xs
+susy17.add("t28tev.chr", '%s/clucas//ICF/automated/2012_08_01_11_43_66/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
+
+susy17.add("t2bb.job418", '%s/yeshaq//ICF/automated/2012_09_26_22_52_33/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs                                                  
+susy17.add("t1tttt.job442", '%s/yeshaq//ICF/automated/2012_10_04_23_15_25/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
+susy17.add("t1bbbb.job443", '%syeshaq//ICF/automated/2012_10_04_22_34_28/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs                                                  
+susy17.add("t1.job444", '%s/yeshaq//ICF/automated/2012_10_04_23_04_08/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
+susy17.add("t2tt.job445", '%s/yeshaq//ICF/automated/2012_10_04_22_47_35/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
+susy17.add("t2.job446", '%s/yeshaq//ICF/automated/2012_10_06_13_53_17/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
+
+susy17.add("T2bb", '%s/yeshaq//ICF/automated/T2bb/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
+susy17.add("T1bbbb", '%s/yeshaq//ICF/automated/t1bbbb/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
