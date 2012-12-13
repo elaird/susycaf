cd0c47e7439d543ede7d049016e277d5cec125e3
Yossof Eshaq <yossofeshaq@gmail.com>
2012-12-13 10:08:23 -0600

    adding t1bbbb ct10

--- samples/__susy17__.py
+++ samples/__susy17__.py
@@ -13,14 +13,20 @@ susy17.add("t1tttt.chr", '%s/clucas//ICF/automated/2012_06_04_18_26_52/")'%pnfs,
 susy17.add("t28tev.chr", '%s/clucas//ICF/automated/2012_08_01_11_43_66/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
 
 susy17.add("t2bb.job418", '%s/yeshaq//ICF/automated/2012_09_26_22_52_33/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs                                                  
-susy17.add("t1tttt.job442", '%s/yeshaq//ICF/automated/2012_10_04_23_15_25/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
+susy17.add("T1tttt", '%s/yeshaq//ICF/automated/2012_10_04_23_15_25/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
 susy17.add("t1bbbb.job443", '%syeshaq//ICF/automated/2012_10_04_22_34_28/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs                                                  
-susy17.add("t1.job444", '%s/yeshaq//ICF/automated/2012_10_04_23_04_08/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
-susy17.add("t2tt.job445", '%s/yeshaq//ICF/automated/2012_10_04_22_47_35/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
-susy17.add("t2.job446", '%s/yeshaq//ICF/automated/2012_10_06_13_53_17/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
+susy17.add("T1", '%s/yeshaq//ICF/automated/2012_10_04_23_04_08/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
+susy17.add("T2tt", '%s/yeshaq//ICF/automated/2012_10_04_22_47_35/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
+susy17.add("T2", '%s/yeshaq//ICF/automated/2012_10_06_13_53_17/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
 
 susy17.add("T2bb_mrst", '%s/yeshaq//ICF/automated/T2bb/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
 susy17.add("T1bbbb_mrst", '%s/yeshaq//ICF/automated/t1bbbb/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
 
 susy17.add("T2bb", '%s/yeshaq//ICF/automated/T2bb_mstw_2/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
 susy17.add("T1bbbb", '%s/yeshaq//ICF/automated/t1bbbb_mstw_2/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
+
+susy17.add("T2bb_nnpdf", '%s/yeshaq//ICF/automated/2012_11_27_14_01_59/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
+susy17.add("T1bbbb_nnpdf", '%s/yeshaq//ICF/automated/2012_11_27_14_24_14/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
+
+susy17.add("T2bw", '%s/clucas//ICF/automated/2012_11_07_13_30_21/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
+susy17.add("T1bbbb_nnpdf_ct10", '%s/yeshaq//ICF/automated/2012_12_03_10_43_11/")'%pnfs, xs = {"LO":107.5}["LO"]) #dummy xs
