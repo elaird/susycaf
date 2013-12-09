d6a7ba7b0289fd05a20f7749389ff003859692b8
Yossof Eshaq <yossofeshaq@gmail.com>
2013-12-09 08:47:48 -0600

    adding eos qcd samples

--- samples/__qcd17__.py
+++ samples/__qcd17__.py
@@ -1,7 +1,8 @@
 from supy.samples import SampleHolder
-from supy.sites import pnfs
+from supy.sites import pnfs, eos
 qcd17 = SampleHolder()
 pnfs = pnfs()
+eos = eos()
 
 binsXs = [ (   0, 4.9586188E10  ),
            (   5, 4.2639499E10  ),
@@ -24,7 +25,7 @@ binsXs = [ (   0, 4.9586188E10  ),
 for i,(low,xs) in enumerate(binsXs[:-1]) :
     hi = binsXs[i+1][0]
     dir = "QCD_Pt-%d%s_TuneZ2star_8TeV_pythia6.Summer12-PU_S7_START52_V9-v1.AODSIM"%(low, "to%d"%hi if hi else "")
-    qcd17.add("qcd_py6_pt_%d"%low, '%s/clucas//ICF/automated/2012_07_03_11_15_20/%s")'%(pnfs, dir), xs = xs)
+    qcd17.add("qcd_py6_pt_%d_dCache"%low, '%s/clucas//ICF/automated/2012_07_03_11_15_20/%s")'%(pnfs, dir), xs = xs)
 
 bEn = '%s/yeshaq//ICF/automated/2012_09_03_15_56_26/'%pnfs
 qcd17.add("qcd_b_mg_ht_250", '%s/BJets_HT-250To500_8TeV-madgraph.Summer12_DR53X-PU_S10_START53_V7A-v1.AODSIM")'%bEn, xs = 5828.0)
@@ -40,3 +41,10 @@ for i,(low,xs) in enumerate(binsXs[:-1]) :
     if i < 1 : dir = dir.replace("_.QCD", "QCD").replace("AODSIM_","AODSIM")
     if low == 1800.: dir = dir.replace("AODSIM_","AODSIM") 
     qcd17.add("qcd_py6_pt_v2_%d"%low, '%s/agapitos//ICF/automated/2012_09_22_14_28_53/%s")'%(pnfs, dir), xs = xs)
+
+for i,(low,xs) in enumerate(binsXs[4:-1]) :
+    hi = binsXs[i+1][0]
+    dir = "QCD_%d%s/"%(low, "to%d"%hi if hi else "")
+    if low in (300,170) : dir = dir.replace("/","_merged/") 
+    qcd17.add("qcd_py6_pt_%d"% low, '%s/clucas/Parked13/%s")'%(eos, dir), xs = xs)
+
