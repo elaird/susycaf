08fd201c935b059b3af722ac2961891918032fa8
Yossof Eshaq <yossofeshaq@gmail.com>
2014-04-14 08:06:44 -0500

    skimFactor & parton-binned samples

--- samples/__dyll17__.py
+++ samples/__dyll17__.py
@@ -14,3 +14,9 @@ dyll17.add("dyll_HT_10To200_M-10To50", '%s/clucas/Parked13/DYJets_10to200_M_10to
 dyll17.add("dyll_HT_10To200_M-50", '%s/clucas/Parked13/DYJets_10to200/")'% eos, xs = 3503.71*skimFactor)
 dyll17.add("dyll_HT_200To400_M-50", '%s/clucas/Parked13/DYJets_200to400_Combined/")'% eos, xs = 24.29)
 dyll17.add("dyll_HT_400ToInf_M-50", '%s/clucas/Parked13/DYJets_400toinf_Combined/")'% eos, xs = 3.3564)           
+
+dyNChris = "/clucas/ICF/automated/2013_07_30_14_43_01/"
+dyll17.add("dy1ll_M-50", '%s/%s/DY1JetsToLL_M-50_TuneZ2Star_8TeV-madgraph.Summer12_DR53X-PU_S10_START53_V7A-v1.AODSIM/")'   %(pnfs,dyNChris), xs= 629.19)
+dyll17.add("dy2ll_M-50", '%s/%s/_.DY2JetsToLL_M-50_TuneZ2Star_8TeV-madgraph.Summer12_DR53X-PU_S10_START53_V7A-v1.AODSIM/")' %(pnfs,dyNChris), xs = 223.32)
+dyll17.add("dy3ll_M-50", '%s/%s/_.DY3JetsToLL_M-50_TuneZ2Star_8TeV-madgraph.Summer12_DR53X-PU_S10_START53_V7A-v1.AODSIM/")' %(pnfs,dyNChris), xs = 62.93)
+dyll17.add("dy4ll_M-50", '%s/%s/_.DY4JetsToLL_M-50_TuneZ2Star_8TeV-madgraph.Summer12_DR53X-PU_S10_START53_V7A-v1.AODSIM/")' %(pnfs,dyNChris), xs = 28.43)
